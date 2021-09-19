b02612044f934f928e349e3fdf8d2b888e43d9b3
Ruslanus00 <90270667+Ruslanus00@users.noreply.github.com>
2021-09-20 00:09:16 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let FirstSphere = 0
+basic.forever(function () {
+    FirstSphere = 20
+    if (FirstSphere == 1) {
+        basic.showString("1")
+    }
+    basic.pause(100)
+    if (FirstSphere == 2) {
+        basic.showString("2")
+    }
+    basic.pause(100)
+    if (FirstSphere == 3) {
+        basic.showString("3")
+    }
+})
